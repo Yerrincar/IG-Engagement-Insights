2103200682b83befa6462b102911fff623aae0fd
Yerrincar <rinconcardosoyeray@gmail.com>
2025-04-11 17:51:04 +0200

    Dashboard versión final 1 creado. Problemas con ciertas columnas arreglado

--- ANALYSIS_IG_ACCOUNT/SQL/query.sql
+++ ANALYSIS_IG_ACCOUNT/SQL/query.sql
@@ -1,5 +1,24 @@
+--Para importar los datos desde psql:
+-- \copy CT_SOLO_FINAL_LISTO FROM 'path' WITH (FORMAT CSV, DELIMITER ',', HEADER, ENCODING 'UTF-8'); 
+
+DROP TABLE IF EXISTS ct_solo_final_listo;
+DROP TABLE IF EXISTS ct_clasificacion;
+DROP TABLE IF EXISTS ct_hashtags;
+
 CREATE TABLE CT_SOLO_FINAL_LISTO (
-commentsCount INT, likesCount INT, tipo VARCHAR, hasMusic BOOLEAN, num_hashtags INT, num_images INT, dia_semana VARCHAR, hora TIME, id INT, franja_horaria VARCHAR
+commentsCount INT, 
+likesCount INT, 
+tipo VARCHAR, 
+hasMusic BOOLEAN, 
+num_hashtags INT, 
+num_images INT, 
+dia_semana VARCHAR,
+mes VARCHAR,
+YEAR INT,
+fecha DATE,
+hora TIME,
+id INT, 
+franja_horaria VARCHAR
 );
 
 CREATE TABLE CT_CLASIFICACION (
@@ -7,5 +26,5 @@ id INT, tipo_de_publicacion VARCHAR, tipo_de_producto VARCHAR
 );
 
 CREATE TABLE CT_HASHTAGS (
-hashtag VARCHAR, num_veces INT, mean FLOAT, maximo INT
+hashtag VARCHAR,  count_uso INT, mean NUMERIC, maximo INT
 );
--- ANALYSIS_IG_ACCOUNT/SQL/tabla_final_update.sql
+++ ANALYSIS_IG_ACCOUNT/SQL/tabla_final_update.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS ct_datos_final;
+
 CREATE TABLE ct_datos_final AS SELECT 
 	 f.commentsCount,
     f.likesCount,
@@ -8,6 +9,9 @@ CREATE TABLE ct_datos_final AS SELECT
     f.num_images,
     f.dia_semana,
     f.hora,
+    f.mes,
+    f.year,
+    f.fecha,
     f.id,
     f.franja_horaria,
     c.tipo_de_publicacion,
@@ -16,9 +20,4 @@ FROM CT_SOLO_FINAL_LISTO f
 LEFT JOIN CT_CLASIFICACION c
   ON f.id = c.id
   ORDER BY f.id;
-
---Actualizamos un valor faltante que no se había importado correctamente
-UPDATE ct_datos_final  SET tipo_de_publicacion='promocion', tipo_de_producto='marcapaginas' WHERE ct_datos_final.id=7;
-
---Arreglamos tíldes truncadas
-UPDATE ct_datos_final SET dia_semana = CONVERT_FROM(CONVERT_TO(dia_semana, 'latin1'), 'utf8') WHERE dia_semana LIKE '%Ã%';
+  
